8ca3b87a31b047ade136ef66f352fe8ee9b79d68
salfab <fabio.salvalai@gmail.com>
2011-03-22 21:31:32 +0100

    Added Auto-increment of the build number !

--- OpenSyno/OpenSyno/Properties/AssemblyInfo.cs
+++ OpenSyno/OpenSyno/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenSyno")]
+[assembly: AssemblyTitle("Open Syno")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OpenSyno")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Seesharp.ch")]
+[assembly: AssemblyProduct("Open Syno")]
+[assembly: AssemblyCopyright("Copyright © Seesharp.ch 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
