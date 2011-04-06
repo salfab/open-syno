7b8da2f7210dda644448dacadad947d3b2a64fba
salfab <fabio.salvalai@gmail.com>
2011-04-06 17:07:20 +0200

    First attempt at using a mockable view-model injection for datacontexts via IoC.

---
+++ OpenSyno/OpenSyno/Converters/TypeTypeConverter.cs
@@ -0,0 +1,77 @@
+namespace OpenSyno.Converters
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+
+    /// <summary>
+    /// Provides a type converter to convert <see cref="T:System.Type"/> objects to and from various other representations.
+    /// </summary>
+    public class TypeTypeConverter : System.ComponentModel.TypeConverter
+    {
+        /// <summary>
+        /// Returns whether this converter can convert an object of the given type to the type of this converter, using the specified context.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext"></see> that provides a format context.</param>
+        /// <param name="sourceType">A <see cref="T:System.Type"></see> that represents the type you want to convert from.</param>
+        /// <returns>
+        /// true if this converter can perform the conversion; otherwise, false.
+        /// </returns>
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return ((sourceType == typeof(string)) || base.CanConvertFrom(context, sourceType));
+        }
+
+        /// <summary>
+        /// Converts from.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="culture">The culture.</param>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Type type = Type.GetType((string)value);
+                if (type == null)
+                {
+                    throw new ArgumentException("Type not found : " + value + ".\r\nTry specifying the complete namespace for the desired type.", "value");
+                }
+                return type;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        /// <summary>
+        /// Returns whether this converter can convert the object to the specified type, using the specified context.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext"></see> that provides a format context.</param>
+        /// <param name="destinationType">A <see cref="T:System.Type"></see> that represents the type you want to convert to.</param>
+        /// <returns>
+        /// true if this converter can perform the conversion; otherwise, false.
+        /// </returns>
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return ((destinationType == typeof(string)) || base.CanConvertTo(context, destinationType));
+        }
+
+        /// <summary>
+        /// Converts to.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="culture">The culture.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="destinationType">Type of the destination.</param>
+        /// <returns></returns>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType != typeof(string))
+            {
+                return base.ConvertTo(context, culture, value, destinationType);
+            }
+            return ((Type)value).AssemblyQualifiedName;
+        }
+    }
+
+}
---
+++ OpenSyno/OpenSyno/Helpers/ViewModelResolver.cs
@@ -0,0 +1,37 @@
+﻿namespace OpenSyno.Helpers
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+
+    using OpenSyno.Converters;
+    using OpenSyno.ViewModels;
+
+
+    public class ViewModelResolver
+    {
+        // Note : Too bad, TypeNameConverter does not exist for WP7 :(
+        // http://msdn.microsoft.com/en-us/library/system.configuration.typenameconverter.aspx
+
+        [TypeConverter(typeof(TypeTypeConverter))]
+        public static Type GetViewModelType(DependencyObject obj)
+        {
+            return (Type)obj.GetValue(ViewModelTypeProperty);
+        }
+
+        public static void SetViewModelType(DependencyObject obj, Type value)
+        {
+            obj.SetValue(ViewModelTypeProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ViewModelType.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelTypeProperty =
+            DependencyProperty.RegisterAttached("ViewModelType", typeof(Type), typeof(ViewModelResolver), new PropertyMetadata(ViewModelTypeChanged));
+        
+        private static void ViewModelTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var view = (FrameworkElement)d;
+            view.DataContext = IoC.Current.Resolve<SearchViewModel>();
+        }
+    }
+}
