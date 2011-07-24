33f558453ea066ed12ff6b3066d1be52839edf07
salfab <fabio.salvalai@gmail.com>
2011-07-24 23:05:43 +0200

    fix assembly version  info

--- OpenSyno/OpenSyno/Properties/AssemblyInfo.cs
+++ OpenSyno/OpenSyno/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.*")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
